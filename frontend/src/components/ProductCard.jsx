5813ea3596e3d8e6ea9b60560d21a7ec53102e49
NetharaW <netharanimangi.27@gmail.com>
2025-06-06 23:29:11 -0300

    Activity 3

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -4,11 +4,11 @@ import { FaEdit, FaTrash } from "react-icons/fa";
 
 const ProductCard = ({ product, onEdit, onDelete }) => {
   return (
-    <Card style={{ width: "18rem" }} key={product.id}>
-      <Card.Img variant="top" src={product.image} />
+    <Card style={{ width: "18rem" }} key={product.id} className="mb-5">
+      <Card.Img variant="top" src={product.image} className="product-image"/>
       <Card.Body>
         <Card.Title>{product.title}</Card.Title>
-        <Card.Text>{product.description}</Card.Text>
+        <Card.Text className="product-description flex-grow-1">{product.description}</Card.Text>
 
         <div className="d-flex justify-content-between align-items-center mt-3">
           <span className="fw-bold text-primary">${product.price}</span>
